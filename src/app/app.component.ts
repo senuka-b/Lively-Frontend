e1f3102a90e22d3a587eaebe585649738ef9d183
Senuka Bandara <senukabandara2006@gmail.com>
2025-04-24 23:13:07 +0530

    Implement Project configuration & assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudioComponent } from "./components/studio/studio.component";
+import { StreamComponent } from "./components/stream/stream.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StudioComponent, StreamComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { StreamComponent } from './components/stream/stream.component';
+import { StudioComponent } from './components/studio/studio.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LandingComponent
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignupComponent
+    },
+    {
+        path: "watch/:code",
+        component: StreamComponent
+    },
+    {
+        path: "studio",
+        component: StudioComponent
+    }
+];
