55e64390ae741f414c9c30ed0e0ee22c132740df
Senuka Bandara <senukabandara2006@gmail.com>
2025-04-25 22:32:03 +0530

    Fix JWT authentication stream connection

--- src/app/interceptors/jwt-interceptor.service.ts
+++ src/app/interceptors/jwt-interceptor.service.ts
@@ -1,6 +1,6 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { TokenStorageService } from '../service/token-storage/token-storage.service';
 import { Router } from '@angular/router';
 
@@ -12,26 +12,33 @@ export class JwtInterceptorService implements HttpInterceptor {
   constructor(private tokenStorageService: TokenStorageService, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.tokenStorageService.getToken();
+    const currentUserToken = this.tokenStorageService.getToken();
 
-    if (token) {
-      const clonedReq = req.clone({
-        setHeaders: { Authorization: `Bearer ${token}` },
-      });
-
-      return next.handle(clonedReq).pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            // Token is invalid or ecpired
-
-            this.router.navigate(['/login']);
-          }
-          return throwError(() => new Error(error.message));
-        })
-      );
-    } else {
-      return next.handle(req);
+    if (req.url.includes('/api/auth/login') || req.url.includes('/api/auth/signup')) {
+      return next.handle(req); // Bypass interceptor
     }
+
+
+    console.log("Intercepting request...");
+    
+
+    if (currentUserToken) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUserToken}`,
+        },
+      });
+    } else {
+    
+      console.log('No token found, redirecting to login...');
+
+
+        this.router.navigate(['/login']);
+    
+      
+    }
+
+    return next.handle(req);
   }
 
 
